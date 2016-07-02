9e991d295921b8930f9c3e9ab56fa2f9719508cd
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 00:18:46 +0900

    exercise 7_6   - 자손 클래스의 생성자에서 부모 클래스의 생성자를 호출해야 하는 이유는 ?

---
+++ src/main/java/exercise7/Exercise7_6.java
@@ -0,0 +1,13 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_6 {
+    /**
+     * 자손 클래스의 생성자에서 부모 클래스의 생성자를 호출해야 하는 이유는 ?
+     *
+     * 부모 클래스의 멤버변수들을 초기화 시켜야 해서?
+     * TODO : 답지 확인
+     */
+}
