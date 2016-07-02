c15be85ef1723d29f96a1758abdf94a29bf80504
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 00:30:35 +0900

    exercise 7_10   - 접근 제어자 변경하고 getter, setter 구현

---
+++ src/main/java/exercise7/Exercise7_10.java
@@ -0,0 +1,50 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_10 {
+    public static void main(String args[]) {
+        MyTv2 t = new MyTv2();
+        t.setChannel(10);
+        System.out.println("CH:" + t.getChannel());
+        t.setVolume(20);
+        System.out.println("VOL:" + t.getVolume());
+    }
+}
+
+
+class MyTv2 {
+    final int MAX_VOLUME = 100;
+    final int MIN_VOLUME = 0;
+    final int MAX_CHANNEL = 100;
+    final int MIN_CHANNEL = 1;
+    private boolean isPowerOn;
+    private int channel;
+    private int volume;
+
+    public boolean isPowerOn() {
+        return isPowerOn;
+    }
+
+    public void setPowerOn(boolean powerOn) {
+        isPowerOn = powerOn;
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
+
