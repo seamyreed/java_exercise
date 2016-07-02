56dd2ddc6e0859348fce258f718affab9b0f38d1
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 00:50:54 +0900

    excercise 7_13   - Math 클래스가 private 인 이유는!

--- src/main/java/exercise7/Exercise7_12_13.java
+++ src/main/java/exercise7/Exercise7_12_13.java
@@ -3,13 +3,22 @@ package exercise7;
 /**
  * Created by hyojeongyoon on 2016. 7. 3..
  */
-public class Exercise7_12 {
+public class Exercise7_12_13 {
 
     /**
+     * 7_12
      * 접근제어자에 대한 설명이 옳지 않은 것은?
      *
      * c,
      * 정정 c. - 지역변수에서는 접근 제어자를 쓸 수 없다.
      *
+     */
+
+    /**
+     * 7_13
+     *
+     * Math 클래스의 접근 제어자가 private 인 이유는 ?
+     *
+     * 인스턴스 생성을 막기 위해서.
      */
 }
