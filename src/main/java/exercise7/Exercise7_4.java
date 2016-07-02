9ffa5f335bc6970e31201a4151c5e3e8b2b35ab9
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 00:05:12 +0900

    exercise 7_4   - 오버라이딩의 조건

---
+++ src/main/java/exercise7/Exercise7_4.java
@@ -0,0 +1,14 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_4 {
+    /**
+     * 오버라이딩의 조건으로 옳지 않은 것은?
+     *
+     * c. d.
+     * 정정 c - 접근 제어자는 조상의 메서드 보다 같거나 넓은 범로만 변경 가능. 좁을 수 없음.
+     * 정정 d - 더 많은 수의 예외를 던질 수 있었던가.. TODO : 모르겠넹. 어휴.
+     */
+}
