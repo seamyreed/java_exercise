8bd3ca37539dab042ca6cb4c5bf2cc6193ddc059
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 01:15:32 +0900

    exercise 7_16

---
+++ src/main/java/exercise7/Exercise7_16.java
@@ -0,0 +1,12 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_16 {
+    /**
+     * 연산 결과 true 가 아닌것은?
+     *
+     * e. fe instanceOf Ambulance
+     */
+}
