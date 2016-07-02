c00dede806712beb8632e221fb8a3a8155662873
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 00:49:06 +0900

    exercise 7_13   - 접근제어자 문제

---
+++ src/main/java/exercise7/Exercise7_12.java
@@ -0,0 +1,15 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_12 {
+
+    /**
+     * 접근제어자에 대한 설명이 옳지 않은 것은?
+     *
+     * c,
+     * 정정 c. - 지역변수에서는 접근 제어자를 쓸 수 없다.
+     *
+     */
+}
