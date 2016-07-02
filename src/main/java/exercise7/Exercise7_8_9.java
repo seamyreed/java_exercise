caa5e559349b6b99d877a1f6086b73568e328a5b
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 00:28:09 +0900

    exercise 7_8_9   - 접근자 문제

---
+++ src/main/java/exercise7/Exercise7_8_9.java
@@ -0,0 +1,26 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_8_9 {
+
+    /**
+     * 7_8
+     *
+     * 접근 제어자를 범위가 넓은 것에서 좁은 것으로 나열
+     *
+     * a. public-protected-default-price
+     *
+     */
+
+    /**
+     * 7_9
+     *
+     * final 을 붙였을 때 대상과 의미가 잘못된 것은?
+     * b, c
+     *
+     * 정정 - b. 클래스 - 상속 할 수 없다.
+     * 정정 - c. 메서드 - 오버라이딩을 할 수 없다.
+     */
+}
