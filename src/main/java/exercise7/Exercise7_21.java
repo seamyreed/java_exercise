5940c964696dbacadbf11a7f72772e65e6edc20f
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 14:28:56 +0900

    exercise 7_21    - 매개변수로 가능한 것은?

---
+++ src/main/java/exercise7/Exercise7_21.java
@@ -0,0 +1,21 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_21 {
+    /**
+     * 다음과 같이 attack 메서드 정의되어 있을 때, 이 메서드의 매개변수로 가능한 것
+     * 두가지
+     *
+     * Movable 인터페이스를 구현한 클래스
+     * TODO : 하나는 뭐지?
+     */
+
+    void attack(Movable f) {
+    }
+}
+
+interface Movable {
+    void move(int x, int y);
+}
