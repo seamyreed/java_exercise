c2fb0ea9045d7e97084a62129ca940adb6995e7b
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 20:20:07 +0900

    exercise 8_3   - 예외처리 오버라이딩이 잘못된 것은?

---
+++ src/main/java/exercise8/Exercise8_3.java
@@ -0,0 +1,13 @@
+package exercise8;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise8_3 {
+    /**
+     *
+     *
+     * d. e.
+     * 던질 수 있는 예외클래스의 상위클래스로 예외를 던질 수 없다.
+     */
+}
