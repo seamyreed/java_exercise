c0952f5cdae9c70f8d3e9bf5f7619c34d75c114d
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 15:07:25 +0900

    exercise 7_24

---
+++ src/main/java/exercise7/Exercise7_24.java
@@ -0,0 +1,14 @@
+package exercise7;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise7_24 {
+    /**
+     * 인터페이스의 장점이 아닌 것은?
+     *
+     * 독립적인 프로그래밍이 가능하다.
+     * TODO : 독립적인 프로그래밍을 가능하게 해준다는 말은 무슨말이지;;
+     * 나머지는 다 맞는거 같은데 흠..
+     */
+}
