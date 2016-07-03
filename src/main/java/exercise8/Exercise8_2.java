c146e6b258d693b535eb89e6140f0df7cd35d8c2
hyo jeong yoon <hyojeongyoon@hyoui-MacBook-Pro.local>
2016-07-03 20:16:52 +0900

    exercise 8_2   - 예외가 발생한 로그 분석하기

---
+++ src/main/java/exercise8/Exercise8_2.java
@@ -0,0 +1,13 @@
+package exercise8;
+
+/**
+ * Created by hyojeongyoon on 2016. 7. 3..
+ */
+public class Exercise8_2 {
+    /**
+     * 예외가 발생한 로그를 보고 옳지 않은 설명을 고르시오
+     *
+     * d.
+     * 정정 d. method1이 method2를 호출한 것이다.
+     */
+}
